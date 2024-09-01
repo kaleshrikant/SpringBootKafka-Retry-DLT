f023617f4ef501771a384ee0edfb4cf12bfa3c59
Shrikant Kale <kaleshrikant@live.com>
2024-09-01 16:05:30 +0530

    Initial Commit

---
+++ SpringBootKafka_Retry_DLT/src/main/java/com/javatechie/SpringBootKafka_Retry_DLT/SpringBootKafkaRetryDltApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.SpringBootKafka_Retry_DLT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaRetryDltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaRetryDltApplication.class, args);
+	}
+
+}
---
+++ SpringBootKafka_Retry_DLT/src/test/java/com/javatechie/SpringBootKafka_Retry_DLT/SpringBootKafkaRetryDltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.SpringBootKafka_Retry_DLT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaRetryDltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
